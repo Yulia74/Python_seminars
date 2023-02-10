366253cef139748040e9e88fd70aa9b8ec8076b1
[Yulia Lebedeva] <yl.75@mail.ru>
2023-02-11 00:20:16 +0500

    added 3 solutions of the task

--- sem_04/Task_027.py
+++ sem_04/Task_027.py
@@ -5,22 +5,31 @@
     Определите, сколько различных слов содержится в этом тексте.
 
 Input:
-She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure.
- So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells.
+She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure. 
+So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells.
 
-Output: 11
-'''
+Output: 17
 
-line = (input('Введите символы строки через пробел: ').lower().split()
-        )  # вводим строку,
+1 вариант решения через множество
+
+line = (input('Введите символы строки через пробел: ').lower().split())  # вводим строку,
 # lower переводит символы в нижний регистр, split разделят список по пробелам (по умолчанию)
 words = set()         # создаем пустое множенство
 for i in line:
     words.add(i)    # заполняем множество уникальными словами
 print(len(words))   # выводим количество элементов в множестве
 
-'''
-решение, записанное в одну строку
+
+2 вариант решения, записанное в одну строку
 
 print(len(set(input('Введите текст ').upper().replace('.', ' ').split())))
+
+
+3 вариант - решение через словарь
 '''
+
+list = input('Введите текст: ').split()
+dict = {}
+for i in list:
+    dict[i.lower()]=0
+print(len(dict.keys()))
